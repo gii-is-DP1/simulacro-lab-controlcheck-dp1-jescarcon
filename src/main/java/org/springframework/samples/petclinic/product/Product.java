d512f83e6aed53a01c55b679cdbd90404a9a6402
jescarcon <jesuscardenas2000@gmail.com>
2022-11-03 07:53:32 +0100

    Ejercicios 1-6

--- src/main/java/org/springframework/samples/petclinic/product/Product.java
+++ src/main/java/org/springframework/samples/petclinic/product/Product.java
@@ -1,13 +1,28 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
+
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
-public class Product {
-    Integer id;
+public class Product extends BaseEntity{
+    @NotEmpty
+    @Min(3)
+    @Max(50)
     String name;
+    @Min(0)
     double price;
+    @ManyToOne
     ProductType productType;
 }
+ 
--- src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
@@ -3,14 +3,21 @@ package org.springframework.samples.petclinic.product;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-
-
-public interface ProductRepository {
+@Repository
+public interface ProductRepository extends CrudRepository<Product,Integer>{
+   
     List<Product> findAll();
     List<ProductType> findAllProductTypes();
     Optional<Product> findById(int id);
     Product findByName(String name);
     Product save(Product p);
+
+    @Query("SELECT t FROM ProductType t WHERE t.name=:name")
+    public ProductType findTypeByName(@Param("name") String name);
 }
+ 
